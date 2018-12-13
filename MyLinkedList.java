6cbcab00598511bd5dc4a4e87ba54a79ff7e8a9a
Tyler Huang <thuang00@stuy.edu>
2018-12-13 11:34:21 -0500

    commentss

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -10,29 +10,37 @@ public class MyLinkedList{
     private Node(int num){
       data = num;
     }
+    //returns the next node
     private Node next(){
       return next;
     }
+    //return the previous node
     private Node prev(){
       return prev;
     }
+    //sets next node to param other
     private void setNext(Node other){
       next = other;
     }
+    //sets prev node to param other
     private void setPrev(Node other){
       prev = other;
     }
+    //return data
     private int getData(){
       return data;
     }
+    //set data to i
     private int setData(int i){
-      data = i;
+      return data = i;
     }
-    private String toString(){
+    //return string representation of node
+    public String toString(){
       return data + "";
     }
   }
 
+  
   public String toString(){
     return "";
   }
